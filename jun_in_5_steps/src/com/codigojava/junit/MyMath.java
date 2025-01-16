d4582598da0e6f79c4381b2ef03a09b327a663e6
Cyb3rh4ck <cyb3rh4ck@outlook.com>
2025-01-16 08:58:10 -0600

    junint in 5 minutes

---
+++ jun_in_5_steps/src/com/codigojava/junit/MyMath.java
@@ -0,0 +1,14 @@
+package com.codigojava.junit;
+
+public class MyMath {
+	//{1,2,3} => 1+2+3 = 6
+	
+	public int sum(int [] numbers) {
+		int sum = 0;
+		for(int number : numbers) {
+			sum += sum + number;
+		}
+		
+		return sum;
+	}
+}
